6270b894af69e5a70713337e60b839a0c549a071
brianjdowling101 <77179492+brianjdowling101@users.noreply.github.com>
2025-10-26 15:15:31 +0000

    Add script.js and options to javascript code.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Define an array of game options
+
+const options = ["rock", "paper", "scissors", "lizard", "spock"];
+
+// Initialize player and computer scores
+let playerScore = 0;
+let computerScore = 0;
