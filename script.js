844bfbcc4891c41b1e8b3517df8d50c6c5d0d24a
brianjdowling101 <77179492+brianjdowling101@users.noreply.github.com>
2025-10-26 15:51:08 +0000

    Add scores and display result based on player choice to javascript.

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ function playRound(playerSelection, computerSelection) {
     const playerIndex = options.indexOf(playerSelection);
     const computerIndex = options.indexOf(computerSelection);
 }
+// Determine the result of the round based on the difference in indexes
+const resultIndex = (playerIndex - computerIndex + 5) % 5;
+switch (resultIndex) {
+    // If indexes are equal, the round is a tie
+    case 0:
+        return "Tie!";
+    // If the difference in indexes is 1 or 3, the player wins the round
+    case 1:
+    case 3:
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        // If the player's score reaches 10, display win message and disable options
+        if (playerScore === 10) {
+            resultDisplay.textContent = "You win!";
+            disableOptions();
+            showPlayAgainButton();
+        }
+}
